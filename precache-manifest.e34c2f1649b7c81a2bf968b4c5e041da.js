f9683268f6b88f2a232ebb65d20361c05e984222
Raphael Passos <raphael251@hotmail.com>
2019-05-21 18:35:55 -0300

    Updates

---
+++ precache-manifest.e34c2f1649b7c81a2bf968b4c5e041da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51c952d89714fd401e4d6399f0bba22",
+    "url": "/virada-cultural/index.html"
+  },
+  {
+    "revision": "3066240f163985ae1c8a",
+    "url": "/virada-cultural/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "07e99b3e2e93930f06fe",
+    "url": "/virada-cultural/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3066240f163985ae1c8a",
+    "url": "/virada-cultural/static/js/2.b765de56.chunk.js"
+  },
+  {
+    "revision": "07e99b3e2e93930f06fe",
+    "url": "/virada-cultural/static/js/main.d635474f.chunk.js"
+  },
+  {
+    "revision": "e8bb543b2c1fd38a8471",
+    "url": "/virada-cultural/static/js/runtime~main.f3de4acf.js"
+  }
+]);
