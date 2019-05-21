818c53ee8ed774389a3f16f114270328b53ab102
Raphael Passos <raphael251@hotmail.com>
2019-05-21 18:09:29 -0300

    first working version

--- src/App.js
+++ src/App.js
@@ -1,77 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import data from "./db/data";
+import Form from "./components/Form";
+import "./App.css";
 
-import data from './db/data';
+const App = () => {
+  const [filter, setFilter] = useState({
+    regiao: undefined,
+    local: undefined,
+    dia: undefined,
+    inicio: undefined
+  });
 
-import Form from './components/Form';
+  const [filteredData, setFilteredData] = useState(data);
 
-import './App.css';
+  useEffect(() => { 
+    console.log('filter: ', filter);
+    
+    const filteredResult = filterResult(filter);
 
-class App extends React.Component {
+    setFilteredData(filteredResult)
+  }, [filter]);
 
-  getRegioes = () => {
-    return data
-      .map(item => (item.regiao))
-      .filter((item, index, arr) => (arr.indexOf(item) === index));
+  return (
+    <div className="container">
+      <Form
+        filteredData={filteredData}
+        getRegioes={getRegioes}
+        getLocais={getLocais}
+        getDias={getDias}
+        getInicios={getInicios}
+        setFilter={setFilter}
+        filter={filter}
+      />
+      <table className="table table-striped table-bordered table-hover table-sm">
+        <thead>
+          <tr>
+            <th>Região</th>
+            <th>Local</th>
+            <th>Dia</th>
+            <th>Horário</th>
+            <th>Categoria</th>
+            <th>Evento</th>
+            <th>Inclusão</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={`evento#${index}`}>
+              <td>{item.regiao}</td>
+              <td>{item.local}</td>
+              <td>{item.dia}</td>
+              <td>{`${item.inicio} - ${item.termino}`}</td>
+              <td>{item.categoria}</td>
+              <td>{item.titulo}</td>
+              <td>{item.acessibilidade ? item.acessibilidade : "-"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const filterResult = (filter) => {
+  let result = data;
+
+  if (filter.regiao !== undefined) {
+    result = result
+      .filter(item => (item.regiao === filter.regiao));
+  }
+  if (filter.local !== undefined) {
+    result = result
+      .filter(item => (item.local === filter.local));
+  }
+  if (filter.dia !== undefined) {
+    result = result
+      .filter(item => (item.dia === filter.dia));
+  }
+  if (filter.inicio !== undefined) {
+    result = result
+      .filter(item => (item.inicio === filter.inicio));
   }
 
-  getLocais = () => {
-    return data
-      .map(item => (item.local))
-      .filter((item, index, arr) => (arr.indexOf(item) === index));
-  }
+  return result;
+};
 
-  getDias = () => {
-    return data
-      .map(item => (item.dia))
-      .filter((item, index, arr) => (arr.indexOf(item) === index));
-  }
+const getRegioes = json => {
+  return json
+    .map(item => item.regiao)
+    .filter((item, index, arr) => arr.indexOf(item) === index);
+};
 
-  getInicios = () => {
-    return data
-      .map(item => (item.inicio))
-      .filter((item, index, arr) => (arr.indexOf(item) === index));
-  }
+const getLocais = json => {
+  return json
+    .map(item => item.local)
+    .filter((item, index, arr) => arr.indexOf(item) === index);
+};
 
-  render() {
-    // console.log('regioes', this.getRegioes())
-    // console.log('locais', this.getLocais())
-    // console.log('dias', this.getDias())
-    // console.log('inicios', this.getInicios())
-    return (
-      <div className="container">
-        <Form 
-          getRegioes={this.getRegioes} 
-          getLocais={this.getLocais} 
-          getDias={this.getDias} 
-          getInicios={this.getInicios} />
-          <table className="table table-striped table-bordered table-hover">
-            <thead>
-              <tr>
-                <th>Região</th>
-                <th>Local</th>
-                <th>Dia</th>
-                <th>Horário</th>
-              </tr>
-            </thead>
-            <tbody>
-              { data
-                .map((item, index) => (
-                    <tr key={ `evento#${index}` } >
-                      <td>{ item.regiao }</td>
-                      <td>{ item.local }</td>
-                      <td>{ item.dia }</td>
-                      <td>{ `${item.inicio} - ${item.termino}` }</td>
-                    
-                      {/* `${ item.regiao } - ${ item.local }: ${ item.dia } ${ item.inicio }-${ item.termino }: ${ item.titulo } ` */}
-                    </tr> 
-                  )
-                ) 
-              }
-            </tbody>
-          </table>
-      </div>
-    );
-  }
-}
+const getDias = json => {
+  return json
+    .map(item => item.dia)
+    .filter((item, index, arr) => arr.indexOf(item) === index);
+};
+
+const getInicios = json => {
+  const unorderedInicioArray = json
+    .map(item => item.inicio)
+    .filter((item, index, arr) => arr.indexOf(item) === index);
+
+  const orderedInicioArray = unorderedInicioArray
+    .map(item => item.split("h")) // Inicio's format: HHhMM (ex.: 00h00 or 23h59)
+    .sort((a, b) => a[0] - b[0])
+    .map(item => item.join("h"));
+
+  return orderedInicioArray;
+};
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,57 @@
 import React from 'react';
 
+const handleAnySelectChange = (event, setFilter) => {
+    const regiao =  document.querySelector('#select-regiao').value === 'undefined' ? undefined : document.querySelector('#select-regiao').value;
+    const local =   document.querySelector('#select-local').value === 'undefined' ? undefined : document.querySelector('#select-local').value;
+    const dia =     document.querySelector('#select-dia').value === 'undefined' ? undefined : document.querySelector('#select-dia').value;
+    const inicio = document.querySelector('#select-horario').value === 'undefined' ? undefined : document.querySelector('#select-horario').value;
+
+    const filter = {
+        regiao,
+        local,
+        dia,
+        inicio
+    };
+
+    setFilter(filter);
+}
+
 const Form = (props) => {
     return (
         <form>
             <div className="row">
                 <div className="col-md-3 form-group">
-                    <label for="select-regiao">Selecione uma região:</label>
-                    <select className="form-control" id="select-regiao" onChange={}>
-                        <option>Todas regiões</option>
-                        { props.getRegioes()
+                    <label htmlFor="select-regiao">Selecione uma região:</label>
+                    <select value={ props.filter.regiao } className="form-control" id="select-regiao" onChange={(e) => handleAnySelectChange(e, props.setFilter)}>
+                        <option value={ 'undefined' }>Todas regiões</option>
+                        { props.getRegioes(props.filteredData)
                             .map((item, index) => (<option key={ `regiao#${ index }` } value={ item }>{ item }</option>))
                         }
                     </select>
                 </div>
                 <div className="col-md-3 form-group">
-                    <label for="select-local">Selecione um local:</label>
-                    <select className="form-control" id="select-local">
-                        <option>Todos locais</option>
-                        { props.getLocais()
+                    <label htmlFor="select-local">Selecione um local:</label>
+                    <select value={ props.filter.local } className="form-control" id="select-local" onChange={(e) => handleAnySelectChange(e, props.setFilter)}>
+                        <option value={ 'undefined' }>Todos locais</option>
+                        { props.getLocais(props.filteredData)
                             .map((item, index) => (<option key={ `local#${ index }` } value={ item }>{ item }</option>))
                         }
                     </select>
                 </div>
                 <div className="col-md-3 form-group">
-                    <label for="select-dia">Selecione um dia:</label>
-                    <select className="form-control" id="select-dia">
-                        <option>Todos dias</option>
-                        { props.getDias()
+                    <label htmlFor="select-dia">Selecione um dia:</label>
+                    <select value={ props.filter.dia } className="form-control" id="select-dia" onChange={(e) => handleAnySelectChange(e, props.setFilter)}>
+                        <option value={ 'undefined' }>Todos dias</option>
+                        { props.getDias(props.filteredData)
                             .map((item, index) => (<option key={ `dia#${ index }` } value={ item }>{ item }</option>))
                         }
                     </select>
                 </div>
                 <div className="col-md-3 form-group">
-                    <label for="select-horario">Selecione um horário:</label>
-                    <select className="form-control" id="select-horario">
-                        <option>Todos horários</option>
-                        { props.getInicios()
+                    <label htmlFor="select-horario">Selecione um horário:</label>
+                    <select value={ props.filter.inicio } className="form-control" id="select-horario" onChange={(e) => handleAnySelectChange(e, props.setFilter)}>
+                        <option value={ 'undefined' }>Todos horários</option>
+                        { props.getInicios(props.filteredData)
                             .map((item, index) => (<option key={ `inicio#${ index }` } value={ item }>{ item }</option>))
                         }
                     </select>
